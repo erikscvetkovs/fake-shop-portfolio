c92d22c514e5b44d8961e622acbe89af01cc0585
ErikCvetkov <58137575+ErikCvetkov@users.noreply.github.com>
2022-08-23 19:09:48 +0300

    finished cart
    quantity for every item in cart

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -8,12 +8,11 @@ import { changeCartState } from '../../features/cart/cartSlicer'
 export default function Cart() {
     const [fadeOut, changeFadestatuss] = useState(true)
     const orders = useSelector((state) => state.orders)
-    const cartState = useSelector((state) => state.cart.isOpen)
     const dispatch = useDispatch();
     const cartBox = useRef(null);
     outsideCartBox(cartBox);
 
-    function outsideCartBox(ref){
+    function outsideCartBox(ref) {
         useEffect(() => {
             function handleClickOutside(event) {
                 if (ref.current && !ref.current.contains(event.target)) {
@@ -27,7 +26,7 @@ export default function Cart() {
         }, [ref]);
     }
 
-    function fadeOutCart(){
+    function fadeOutCart() {
         changeFadestatuss(false)
         setTimeout(() => {
             dispatch(changeCartState(false))
@@ -39,50 +38,67 @@ export default function Cart() {
         <div className={`${fadeOut ? 'fadeIn' : 'fadeOut'} cart-overlay`}>
             <div className='cart-box'>
                 <Col sm={5} className='cart-menu' ref={cartBox}>
-                    <div className='cart-close-button' onClick={() => {fadeOutCart()}}>
+                    <div className='cart-close-button' onClick={() => { fadeOutCart() }}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                             <path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" />
                         </svg>
                     </div>
-                    <Row>
-                        <Col className='cart-heading'>
-                            My bag, <span className='cart-heading-count'>{orders.items.length} items</span>
-                        </Col>
-                    </Row>
-                    <div className='orders-box'>
-                        {orders.items.map((order) => {
-                            return (
-                                <Row className='order-row'>
-                                    <Col className='cart-order-image' sm={4}>
-                                        <div className='order-image' style={{ backgroundImage: `url("${order.image}")` }}></div>
-                                    </Col>
-                                    <Col className='order-info'>
-                                        <Row>
-                                            <Col className='cart-order-name'>{order.title}</Col>
+                    {orders.items.length === 0 ? (
+                        <Row>
+                            <Col className='cart-heading empty'>
+                                Cart is empty
+                            </Col>
+                        </Row>
+                    ) : (
+                        <>
+                            <Row>
+                                <Col className='cart-heading'>
+                                    My bag, <span className='cart-heading-count'>{orders.quantity} items</span>
+                                </Col>
+                            </Row>
+                            <div className='orders-box'>
+                                {orders.items.map((order) => {
+                                    console.log(order)
+                                    return (
+                                        <Row key={order.id} className='order-row'>
+                                            <Col className='cart-order-image' sm={4}>
+                                                <div className='order-image' style={{ backgroundImage: `url("${order.image}")` }}></div>
+                                            </Col>
+                                            <Col className='order-info'>
+                                                <Row>
+                                                    <Col className='cart-order-name'>{order.title}</Col>
+                                                </Row>
+                                                <Row>
+                                                    <Col className='cart-order-price'>{order.price} $</Col>
+                                                </Row>
+                                                <Row>
+                                                    <Col className='cart-order-quantity'>Quantity: {order.quantity}</Col>
+                                                </Row>
+                                            </Col>
                                         </Row>
-                                        <Row>
-                                            <Col className='cart-order-price'>{order.price} $</Col>
-                                        </Row>
-                                        <Row>
-                                            <Col className='cart-order-quantity'>Quantity: 1</Col>
-                                        </Row>
-                                    </Col>
-                                </Row>
-                            )
-                        })}
-                    </div>
-                    <Row className='cart-buttons'>
-                        <Col>
-                            <div className='btn-cart btn-view-bag'>
-                                View bag
+                                    )
+                                })}
                             </div>
-                        </Col>
-                        <Col>
-                            <div className='btn-cart btn-checkout'>
-                                Checkout
-                            </div>
-                        </Col>
-                    </Row>
+                            <Row>
+                                <Col className='cart-total-sum'>
+                                    <span>Total: </span>
+                                    <span className='total-sum'> {orders.totalSum} $</span>
+                                </Col>
+                            </Row>
+                            <Row className='cart-buttons'>
+                                <Col>
+                                    <div className='btn-cart btn-view-bag'>
+                                        View bag
+                                    </div>
+                                </Col>
+                                <Col>
+                                    <div className='btn-cart btn-checkout'>
+                                        Checkout
+                                    </div>
+                                </Col>
+                            </Row>
+                        </>
+                    )}
                 </Col>
             </div>
         </div>
--- src/features/order/orderSlice.js
+++ src/features/order/orderSlice.js
@@ -1,15 +1,56 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    items: []
+    items: [],
+    quantity: 0,
+    totalSum: 0
+}
+
+
+function getQuantity (items) {
+    let quantity = 0
+    items.forEach((item) => {
+        quantity += item.quantity
+    })
+    return quantity
+}
+
+function getTotalSum (items) {
+    let sum = 0
+    items.forEach((item) => {
+        sum += item.price
+    })
+    return sum
+}
+
+function isInArray(item, orders) {
+    let indexInArray = null
+
+    const isFound = orders.some((element, index) => {
+        if (element.id === item.id) {
+            indexInArray = index;
+            return true;
+        }
+        return false;
+    });
+
+    return [isFound, indexInArray]
 }
 
 export const orderSlice = createSlice({
     name: 'orders',
     initialState,
-    reducers:  {
-        addOrder: (state,action) =>{
-            state.items.push(action.payload)
+    reducers: {
+        addOrder: (state, action) => {
+            if (!isInArray(action.payload, state.items)[0]) {
+                const item = JSON.parse(JSON.stringify(action.payload))
+                item.quantity = 1
+                state.items.push(item)
+            } else {
+                state.items[isInArray(action.payload, state.items)[1]].quantity++
+            }
+            state.quantity = getQuantity(state.items)
+            state.totalSum = getTotalSum(state.items)
         }
     }
 })
